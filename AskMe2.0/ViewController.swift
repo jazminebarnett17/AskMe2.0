5521041981e799bdbb09835712a9007f03e77130
Jazmine <jazminebarnett17@augustana.edu>
2021-07-22 14:07:14 -0500

    Ask me anything
    Ask me anything

--- AskMe2.0/ViewController.swift
+++ AskMe2.0/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var butterflyImageView: UIImageView!
+    var answerArray = ["1","2","3","4", "5"]
+    var randomAnswerNum = 0
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askButton(_ sender: Any) {
+    randomAnswerNumber()
+    }
+    
+    func randomAnswerNumber() {
+        randomAnswerNum = Int.random(in: 0...4)
+        butterflyImageView.image = UIImage(named: answerArray[randomAnswerNum])
+    }
 }
 
